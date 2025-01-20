a60b028172b993031239e58a5fd3ef1805ad776f
izibyosevictoire <luandavicky5@gmail.com>
2025-01-20 08:47:51 +0200

    deploying

--- wallet-web-app1/server/server.js
+++ wallet-web-app1/server/server.js
@@ -1,17 +1,20 @@
-
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 
+const app = express();  
 
-app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+app.use(express.json());
+app.use(cors());
 
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.get('/api', (req, res) => {
   res.send('Welcome to the E-Wallet Backend API');
 });
+
 
 const transactionRoutes = require("./api/transactions");
 const budgetRoutes = require("./api/budgets");
@@ -20,8 +23,8 @@ app.use("/api/budgets", budgetRoutes);
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
-});
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  });
 
 
 const PORT = process.env.PORT || 8000;
@@ -29,6 +32,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
