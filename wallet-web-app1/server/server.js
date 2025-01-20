e4ac74948972a4b415a97de197379fec23a2124b
izibyosevictoire <luandavicky5@gmail.com>
2025-01-20 06:35:23 +0200

    deploying

--- wallet-web-app1/server/server.js
+++ wallet-web-app1/server/server.js
@@ -1,18 +1,30 @@
- require ("dotenv").config();
+
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
-const app = express();
-app.use(express.json());
-app.use(cors());
 
-// app.get('/', (req, res) => {
-//   res.send('Welcome to the E-Wallet Backend API');
-// });
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+
+app.get('/api', (req, res) => {
+  res.send('Welcome to the E-Wallet Backend API');
+});
+
+const transactionRoutes = require("./api/transactions");
+const budgetRoutes = require("./api/budgets");
+app.use("/api/transactions", transactionRoutes);
+app.use("/api/budgets", budgetRoutes);
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
+
+
 const PORT = process.env.PORT || 8000;
-
-
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
@@ -21,12 +33,3 @@ mongoose
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-const transactionRoutes = require("./api/transactions");
-const budgetRoutes = require("./api/budgets");
-
-
-
-app.use("/api/transactions", transactionRoutes);
-app.use("/api/budgets", budgetRoutes);
-
